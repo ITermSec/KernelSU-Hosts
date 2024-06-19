60417674e57ece9748361b8fed16edbc34ceb2da
ITermSec <43831201+ITermSec@users.noreply.github.com>
2024-06-19 19:23:54 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,15 +1,16 @@
 PATH=/system/etc
+	ui_print "- Patching 'hosts' file: $PATH"
 	ui_print "- Author : ITermSec"
 	ui_print "- Version : 6.6.6"
 	ui_print "- Description : An efficient ad blocker with no user interface, or without installing third party apps."
-	ui_print "- Patching 'hosts' file : $PATH/hosts"
 mkdir -p $MODPATH$PATH
-mv -f $MODPATH/hosts $MODPATH$PATH
-chcon -r u:object_r:system_file:s0 "$MODPATH/system"
+cp $MODPATH/hosts /system/etc/hosts
+mv $MODPATH/hosts $MODPATH$PATH/hosts
+chcon -r u:object_r:system_file:s0 "$MODPATH/system/etc"
 chmod 644 "$MODPATH/system/etc/hosts"
-chmod 644 "$MODPATH$PATH/hosts"
-chmod 644 "$PATH/hosts"
+chmod 644 "$MODPATH/$PATH/hosts"
 mkdir "$MODPATH/worker"
 touch "$MODPATH/skip_mount"
+
 # Clean up
 rm -rf $MODPATH/hosts
